45f85d2ee315e45ba9e4afb3576d9a9d2455483e
Jakob Schreiner <jakob2602@gmail.com>
2017-11-08 15:02:53 +0100

    multiple meshes subdomains and some recontruction stuff

---
+++ scratch/meshes/meshfunction.py
@@ -0,0 +1,93 @@
+from dolfin import *
+import numpy as np
+from IPython import embed
+import sys
+
+mesh1 = UnitSquareMesh(10, 10)
+mesh2 = UnitSquareMesh(15, 15)
+mesh2.coordinates()[:] += 0.5
+
+joined_cells = np.concatenate((mesh1.cells(), mesh2.cells() + mesh1.cells().max() + 1))
+joined_cells = np.array(joined_cells, dtype=np.uintp)
+joined_coordinates = np.concatenate((mesh1.coordinates(), mesh2.coordinates()))
+
+
+mesh = Mesh()
+editor = MeshEditor()
+
+editor.open(mesh, 2, 2)  # top. and geom. dimension are both 2
+editor.init_vertices(joined_coordinates.shape[0])  # number of vertices
+editor.init_cells(joined_cells.shape[0])     # number of cells
+
+for i, c in enumerate(joined_coordinates):
+    editor.add_vertex(i, c)
+
+for i, c in enumerate(joined_cells):
+    editor.add_cell(i, c)
+
+editor.close()
+
+class Brain(SubDomain):
+    def inside(self, x, on_boundary):
+        return x[0] < 0.5 + 1e-3 or x[1] < 0.5 + 1e-3
+
+
+class Ventricle(SubDomain):
+    def inside(self, x, on_boundary):
+        return x[0] > 0.5 - 1e-3 and x[1] > 0.5 - 1e-3 
+
+
+class Interface(SubDomain):
+    def inside(self, x, on_boundary):
+        return near(x[0], 0.5) and x[1] > 0.5 - 1e-3 \
+            or near(x[1], 0.5) and x[0] > 0.5 - 1e-3
+
+
+brain = Brain()
+ventricle = Ventricle()
+interface = Interface()
+
+
+mf = CellFunction("size_t", mesh)
+mf.set_all(0)
+brain.mark(mf, 1)
+ventricle.mark(mf, 2)
+
+ff = FacetFunction("size_t", mesh)
+interface.mark(ff, 3)
+
+plot(mf)
+input()
+# embed()
+sys.exit(1)
+
+a0 = Constant(1.0)
+a1 = Constant(0.01)
+g_L  = Expression("- 10*exp(-pow(x[1] - 0.5, 2))", degree=2)
+g_R = Constant(1.0)
+f = Constant(1.0)
+
+# Define function space and basis functions
+V = FunctionSpace(mesh, "CG", 2)
+u = TrialFunction(V)
+v = TestFunction(V)
+
+bcs = [DirichletBC(V, 5.0, DomainBoundary())]
+
+ds = Measure("ds", domain=mesh, subdomain_data=ff)
+dx = Measure("dx", domain=mesh, subdomain_data=mf)
+
+F = inner(a0*grad(u), grad(v))*dx(1) + inner(a1*grad(u), grad(v))*dx(2) \
+    - g_L*v*ds(1)
+
+# Separate left and right hand sides of equation
+a, L = lhs(F), rhs(F)
+
+# Solve problem
+u = Function(V)
+solve(a == L, u, bcs)
+
+# Plot solution and gradient
+plot(u, title="u")
+plot(grad(u), title="Projected grad(u)")
+input()
---
+++ scratch/meshes/poisson_subdomain_demo.py
@@ -0,0 +1,130 @@
+from dolfin import *
+
+# Create classes for defining parts of the boundaries and the interior
+# of the domain
+class Left(SubDomain):
+    def inside(self, x, on_boundary):
+        return near(x[0], 0.0)
+
+
+class Right(SubDomain):
+    def inside(self, x, on_boundary):
+        return near(x[0], 1.0)
+
+
+class Bottom(SubDomain):
+    def inside(self, x, on_boundary):
+        return near(x[1], 0.0)
+
+
+class Top(SubDomain):
+    def inside(self, x, on_boundary):
+        return near(x[1], 1.0)
+
+
+class Obstacle(SubDomain):
+    def inside(self, x, on_boundary):
+        return (between(x[1], (0.5, 0.7)) and between(x[0], (0.2, 1.0)))
+
+
+class InsideTop(SubDomain):
+    def inside(self, x, on_boundary):
+        return x[1] > 0.5
+
+
+# Initialize sub-domain instances
+left = Left()
+top = Top()
+right = Right()
+bottom = Bottom()
+obstacle = Obstacle()
+insidetop = InsideTop()
+
+
+# Define mesh
+mesh = UnitSquareMesh(64, 64)
+
+
+# Initialize mesh function for interior domains
+domains = CellFunction("size_t", mesh)
+domains.set_all(0)
+insidetop.mark(domains, 2)
+obstacle.mark(domains, 1)
+
+
+# Initialize mesh function for boundary domains
+boundaries = FacetFunction("size_t", mesh)
+boundaries.set_all(0)
+left.mark(boundaries, 1)
+top.mark(boundaries, 2)
+right.mark(boundaries, 3)
+bottom.mark(boundaries, 4)
+
+# plot(domains)
+# input()
+# 1/0
+
+# Define input data
+a0 = Constant(1.0)
+a1 = Constant(0.01)
+a2 = Constant(0.5)
+g_L = Expression("-10*exp(-pow(x[1] - 0.5, 2))", degree=2)
+g_R = Constant(1.0)
+f = Constant(1.0)
+g = Constant(-0.2)
+
+
+# Define function space and basis functions
+V = FunctionSpace(mesh, "CG", 2)
+u = TrialFunction(V)
+v = TestFunction(V)
+
+
+# Define Dirichlet boundary conditions at top and bottom boundaries
+bcs = [
+    DirichletBC(V, 5.0, boundaries, 2),
+    DirichletBC(V, 0.0, boundaries, 4)
+]
+
+
+# Define new measures associated with the interior domains and
+# exterior boundaries
+dx = Measure("dx", domain=mesh, subdomain_data=domains)
+ds = Measure("ds", domain=mesh, subdomain_data=boundaries)
+
+
+# Define variational form
+F = (
+    inner(a0*grad(u), grad(v))*dx(0) +
+    inner(a1*grad(u), grad(v))*dx(1) -
+    inner(a2*grad(u), grad(v))*dx(2) -
+    g_L*v*ds(1) -
+    g_R*v*ds(3) -
+    f*v*dx(0) -
+    f*v*dx(1) -
+    g*v*dx(2)
+)
+
+
+# Separate left and right hand sides of equation
+a, L = lhs(F), rhs(F)
+
+# Solve problem
+u = Function(V)
+solve(a == L, u, bcs)
+
+# Evaluate integral of normal gradient over top boundary
+n = FacetNormal(mesh)
+m1 = dot(grad(u), n)*ds(2)
+v1 = assemble(m1)
+print ("\int grad(u) * n ds(2) = ", v1)
+
+# Evaluate integral of u over the obstacle
+m2 = u*dx(1)
+v2 = assemble(m2)
+print ("\int u dx(1) = ", v2)
+
+# Plot solution and gradient
+plot(u, title="u")
+plot(grad(u), title="Projected grad(u)")
+input()
