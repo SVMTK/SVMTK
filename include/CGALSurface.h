b24ced57d0b1725798a6e0f8b0001d6f047e28c3
Jakob Schreiner <jakob2602@gmail.com>
2018-02-20 16:12:25 +0100

    added the easy stuff for mesh generator

---
+++ include/CGALSurface.h
@@ -0,0 +1,127 @@
+#ifndef CGALSurface_H
+
+
+#define CGALSurface_H
+
+#ifndef BOOST_PARAMETER_MAX_ARITY
+# define BOOST_PARAMETER_MAX_ARITY 12
+#endif
+
+class Implicit_function;
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <memory>
+#include <array>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/Surface_mesh_default_triangulation_3.h>
+#include <CGAL/Side_of_triangle_mesh.h>
+#include <CGAL/Complex_2_in_triangulation_3.h>
+#include <CGAL/make_surface_mesh.h>
+#include <CGAL/Implicit_surface_3.h>
+#include <CGAL/Surface_mesh_default_criteria_3.h>
+#include <CGAL/IO/Complex_2_in_triangulation_3_file_writer.h>
+#include <CGAL/IO/output_surface_facets_to_polyhedron.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Mesh_polyhedron_3.h>
+
+
+
+
+
+
+class CGALSurface
+{
+   public:
+
+
+
+    typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel; // Change to exact -> copy face_ graph to inexact ??
+    typedef Kernel::Point_3 Point_3;
+    typedef CGAL::Surface_mesh<Point_3> Mesh;
+    typedef CGAL::Surface_mesh_default_triangulation_3 Tr;
+    typedef Tr::Geom_traits GT;
+    typedef Kernel::Vector_3 Vector_3;
+
+
+
+
+    typedef boost::graph_traits<Mesh>::halfedge_descriptor    halfedge_descriptor;
+    typedef boost::graph_traits<Mesh>::face_descriptor        face_descriptor;
+    typedef boost::graph_traits<Mesh>::vertex_descriptor      vertex_descriptor;
+    typedef std::vector<vertex_descriptor>                    vertex_vector;
+    typedef CGAL::Side_of_triangle_mesh<Mesh,Kernel> Inside; //
+
+
+    CGALSurface(const std::string filename);
+
+    CGALSurface( Implicit_function implicit_function,
+                 double bounding_sphere_radius,
+                 double angular_bound,
+                 double radius_bound,
+                 double distance_bound);
+
+
+        void operator^=( CGALSurface& other);
+
+        void operator+=( CGALSurface& other );
+
+        void operator-=( CGALSurface& other );
+
+        int fill_holes();
+
+        bool triangulate_faces();
+      
+        void stitch_borders();
+         
+        void insert_surface(CGALSurface& surface);
+
+        void isotropic_remeshing(double target_edge_length, unsigned int nb_iter, bool protect_border);
+       
+        void adjust_boundary(const double c);
+
+        void smooth_laplacian(const double c);
+
+        template<typename InputIterator>
+        void adjusting_boundary_region(InputIterator begin , InputIterator  end, const double c);
+
+        template<typename InputIterator>
+        void smooth_laplacian_region(InputIterator begin , InputIterator end ,const int c);
+
+        vertex_vector points_inside(CGALSurface& other);
+
+        vertex_vector points_outside(CGALSurface& other);
+
+        Mesh& get_mesh();
+        //Polyhedron& polyhedron();
+
+        bool self_intersections();
+
+        template< typename Polyhedron_3>
+        void get_polyhedron(Polyhedron_3 &polyhedron_3 );
+
+        void save(const char* outpath);
+
+        void preprocess(double target_edge_length, int nb_iter);
+
+
+        void fair(CGALSurface::vertex_vector vector  );
+
+        template<typename Implicit_function>
+        CGALSurface(Implicit_function implicit_function,
+             double bounding_sphere_radius,
+             double angular_bound=30.,
+             double radius_bound=0.1 ,
+             double distance_bound=0.1   );
+
+        ~CGALSurface(){}
+   private :
+       Mesh mesh;
+
+
+};
+
+#endif
--- setup.py
+++ setup.py
@@ -1,20 +1,70 @@
-import platform
+# This setup is borrowed from https://github.com/pybind/cmake_example
+
+import os
+import re
 import sys
+import subprocess
 
 from setuptools import (
     setup,
     find_packages,
+    Extension,
 )
 
+from setuptools.command.build_ext import build_ext
+
+
 # Version number
-major = 0
-minor = 1
+MAJOR = 0
+MINOR = 1
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=""):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(["cmake", "--version"])
+        except OSError:
+            names = ", ".join(e.name for e in self.extensions)
+            msg = f"CMake must be installed to build the following extensions: {names}"
+            raise RuntimeError(msg)
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = [
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
+            f"-DPYTHON_EXECUTABLE={sys.executable}"
+        ]
+
+        cfg = "Debug" if self.debug else "Release"
+        build_args = ["--config", cfg]
+        env = os.environ.copy()
+        env["CXXFLAGSS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get("CXXFLAGS", ""),
+            self.distribution.get_version()
+        )
+
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=self.build_temp)
 
 
 setup(
     name="brainmesh",
-    version="{0}.{1}".format(major, minor),
-    description="A collection of scripts for brain meshing.",
+    version="{0}.{1}".format(MAJOR, MINOR),
+    description="A collection of tools for volume and surface meshing",
+    long_description="",
+    ext_modules=[CMakeExtension("brainmesh")],
+    cmdclass=dict(build_ext=CMakeBuild),
     packages=find_packages("source"),
     package_dir={"": "source"},
     entry_points={
@@ -22,5 +72,6 @@ setup(
             "asc2domain = brainmesh.utils.asc2domain:main",
             "brainmesh-convert = brainmesh.utils.convert:main",
         ]
-    }
+    },
+    zip_safe=False
 )
