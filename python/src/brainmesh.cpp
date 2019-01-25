8c8ec9c75ef3ec868ea4202e55683a67bdd5a108
Jakob Schreiner <jakob2602@gmail.com>
2019-01-25 12:11:04 +0100

    fixed default arguments in reconstruction

--- python/src/brainmesh.cpp
+++ python/src/brainmesh.cpp
@@ -11,13 +11,13 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(brainmesh, m) {
 
-    m.def("reconstruct_surface", &reconstruct_surface);
-            /* , */
-        /* py::arg("sm_angle") = 20.0, */
-        /* py::arg("sm_radius") = 100.0, */
-        /* py::arg("sm_distance") = 0.25, */
-        /* py::arg("approximation_ratio") = 0.02, */
-        /* py::arg("average_spacing_ratio") = 5.0); */
+    m.def("reconstruct_surface", &reconstruct_surface,
+        py::arg("input_surface"),
+        py::arg("sm_angle") = 20.0,
+        py::arg("sm_radius") = 100.0,
+        py::arg("sm_distance") = 0.25,
+        py::arg("approximation_ratio") = 0.02,
+        py::arg("average_spacing_ratio") = 5.0);
 
     py::class_<CGALSurface>(m, "BrainSurface")
         .def(py::init<std::string &>())
@@ -55,12 +55,6 @@ PYBIND11_MODULE(brainmesh, m) {
         /* .def("insert_surface", &CGALSurface::insert_surface) */  // TODO cpp side
         /* .def("getMesh", &CGALSurface::get_mesh) */       // No need to expose
         /* .def("get_polyhedron", &CGALSurface::get_polyhedron); */ // No need to expose
-
-        /* // Experimental reconstruction -- creates self-intersections */
-        /* .def("reconstruct_surface", &CGALSurface::reconstruct_surface, */
-        /*         py::arg("sm_angle") = 20, */
-        /*         py::arg("sm_radius") = 30, */
-        /*         py::arg("sm_distance") = 0.375) */
 
         .def("num_faces", &CGALSurface::num_faces)
         .def("num_edges", &CGALSurface::num_edges)
