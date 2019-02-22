95f9b469b04ca45df005056d3ef919aa42ddc058
larsmva <larsmvalnes@gmail.com>
2019-02-22 11:25:57 +0100

    removed files

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lars/BrainMesh
-# Build directory: /home/lars/BrainMesh
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(external/pybind11)
