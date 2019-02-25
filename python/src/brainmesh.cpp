5c54e0af7dd6501a4d989de643bfc0f59a764d0d
Jakob Schreiner <jakob2602@gmail.com>
2019-02-25 22:20:25 +0100

    typo

--- python/src/brainmesh.cpp
+++ python/src/brainmesh.cpp
@@ -1,11 +1,11 @@
 #include <pybind11/pybind11.h>
-# #include <pybind11/stl_bind.h>
-# #include <pybind11/stl.h>
+//#include <pybind11/stl_bind.h>
+/* #include <pybind11/stl.h> */
 #include <pybind11/functional.h>
 
 #include "CGALSurface.h"
-# #include "CGALMeshCreator.h"
-# #include "surface_mesher.h"
+/* #include "CGALMeshCreator.h" */
+/* #include "surface_mesher.h" */
 
 
 namespace py = pybind11;
