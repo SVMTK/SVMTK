025a091e6ac9af2e74370231f64991f400191633
Jakob Schreiner <jakob2602@gmail.com>
2019-02-25 22:22:56 +0100

    backtracking

--- python/src/brainmesh.cpp
+++ python/src/brainmesh.cpp
@@ -1,6 +1,6 @@
 #include <pybind11/pybind11.h>
-//#include <pybind11/stl_bind.h>
-/* #include <pybind11/stl.h> */
+#include <pybind11/stl_bind.h>
+#include <pybind11/stl.h>
 #include <pybind11/functional.h>
 
 #include "CGALSurface.h"
