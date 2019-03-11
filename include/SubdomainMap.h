0729080357afac6dc81dffb4a288266ff8da9014
Jakob Schreiner <jakob2602@gmail.com>
2019-03-11 09:58:10 +0100

    formatting

--- include/SubdomainMap.h
+++ include/SubdomainMap.h
@@ -1,17 +1,19 @@
 #ifndef SubdomainMap_H
 #define SubdomainMap_H
 
+
 #include <boost/dynamic_bitset.hpp>
-#include <boost/type_traits/remove_reference.hpp>
-#include <boost/type_traits/remove_cv.hpp>
+/* #include <boost/type_traits/remove_reference.hpp> */
+/* #include <boost/type_traits/remove_cv.hpp> */
 #include <map>
 #include <string>
-#include <vector>
+/* #include <vector> */
 #include <iostream>
+
 
 class AbstractMap
 {
-   public:
+    public:
         typedef int return_type;
         typedef boost::dynamic_bitset<> Bmask;
 
@@ -22,47 +24,52 @@ class AbstractMap
 
 class DefaultMap : virtual public AbstractMap
 {
-   public:
+    public:
         typedef int return_type;
         typedef boost::dynamic_bitset<> Bmask;
-    
+
         DefaultMap() {}
         ~DefaultMap() {} 
 
         return_type index(const Bmask bits) 
         {
-           return static_cast<return_type>(bits.to_ulong());
+           return static_cast< return_type >(bits.to_ulong());
         }
 };
 
-class SubdomainMap :virtual public AbstractMap
+
+class SubdomainMap : virtual public AbstractMap
 {
    private:
-        std::map<boost::dynamic_bitset<>,int> subdmap;
+        std::map< boost::dynamic_bitset<>, int > subdmap;
 
    public:
         typedef int return_type;
         typedef boost::dynamic_bitset<> Bmask;
-    
+
         SubdomainMap() {}
         ~SubdomainMap() {} 
 
         void add(std::string string, int subdomain)
         {
-           std::reverse( string.begin(), string.end() ) ; // Bmask initiate reverse 
-           subdmap[Bmask(string)]=subdomain;
-        } 
-        return_type index(const Bmask bits) 
-        {
-           return static_cast<return_type>(subdmap[bits]);  
+           std::reverse(string.begin(), string.end()) ; // Bmask initiate reverse 
+           subdmap[Bmask(string)] = subdomain;
         }
-        void print() 
+
+        return_type index(const Bmask bits)
         {
-           for(std::map<boost::dynamic_bitset<>,int>::iterator it=subdmap.begin();it!=subdmap.end();++it )
-           {
-              std::cout << it->first << " " << it->second << " " << std::endl;
-           }
+           return static_cast< return_type >(subdmap[bits]);
+        }
+
+        void print()
+        {
+           /* for(std::map<boost::dynamic_bitset<>,int>::iterator it=subdmap.begin();it!=subdmap.end(); ++it) */
+            for (const auto tup: subdmap)
+            {
+                std::cout << tup.first << " " << tup.second << " " << std::endl;
+            }
         }
 };
+
 
 #endif
